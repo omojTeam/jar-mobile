b9b172e69cd592e61d7ebc889369da702ffa56d7
MobileDev-1\bkoscielinski <bkoscielinski@seth.software>
2021-03-27 10:11:07 +0100

    Jar & Card DTOs added

---
+++ HappinesJar/HappinesJar/DTO/Cards/CardDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace HappinesJar.DTO.Cards
+{
+    public class CardDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ HappinesJar/HappinesJar/DTO/Jar/JarDTO.cs
@@ -0,0 +1,32 @@
+﻿using HappinesJar.DTO.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace HappinesJar.Jar.DTO
+{
+    public class JarDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("cardsPerDay")]
+        public int CardsPerDay { get; set; }
+
+        [JsonPropertyName("cardsLeftToday")]
+        public int CardsLeftToday { get; set; }
+
+        [JsonPropertyName("cardsLeft")]
+        public int CardsLeft { get; set; }
+
+        [JsonPropertyName("recipientEmail")]
+        public string RecipientEmail { get; set; }
+
+        [JsonPropertyName("cards")]
+        public List<CardDTO> Cards { get; set; }
+    }
+}
