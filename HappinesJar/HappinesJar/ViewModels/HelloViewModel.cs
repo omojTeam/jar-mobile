a54afbb379360df919f396e70052ee8a3c3a7d97
MobileDev-1\bkoscielinski <bkoscielinski@seth.software>
2021-03-27 12:05:35 +0100

    HelloViewModel

---
+++ HappinesJar/HappinesJar/ViewModels/HelloViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using HappinesJar.Utils;
+
+namespace HappinesJar.ViewModels
+{
+    public class HelloViewModel : NotifyPropertyChanged
+    {
+    }
+}
